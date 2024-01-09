d4bcb190f77bc37597221ef4edd6cfe1cc96f0f7
botcoder69 <reodiqueridge10@gmail.com>
2024-01-09 23:31:04 +0800

    Changed icon links inside config to properly apply logo

--- guide/.vuepress/config.ts
+++ guide/.vuepress/config.ts
@@ -15,11 +15,11 @@ const config = defineUserConfig<DefaultThemeOptions, ViteBundlerOptions>({
 	head: [
 		['meta', { charset: 'utf-8' }],
 		['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }],
-		['link', { rel: 'icon', href: '/favicon.png' }],
+		['link', { rel: 'icon', href: '/java-guide/favicon.png' }],
 		['meta', { name: 'theme-color', content: '#3eaf7c' }],
 		['meta', { property: 'og:type', content: 'website' }],
 		['meta', { property: 'og:locale', content: 'en_US' }],
-		['meta', { property: 'og:image', content: '/meta-image.png' }]
+		['meta', { property: 'og:image', content: '/java-guide/meta-image.png' }]
 	],
 	theme: path.join(__dirname, 'theme', 'index.ts'),
 	themeConfig: {
